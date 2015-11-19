b1036e2708932804e34b21d68d4b9f1021389f92
geekan <geekan@foxmail.com>
2015-11-19 11:21:32 +0800

    Create tfidf.py

---
+++ tfidf.py
@@ -0,0 +1,64 @@
+from __future__ import division
+
+import psu
+import math
+import sys
+
+from textblob import TextBlob as tb
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def tf(word, blob):
+    return blob.words_count[word] / blob.words_len
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob.words_count)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
+
+
+def build_words_count(blob):
+    words_count = {}
+    for word in blob.words:
+        if words_count.get(word):
+            words_count[word] += 1
+        else:
+            words_count[word] = 1
+
+    return words_count
+
+files = psu.ls()
+files = filter(lambda x: x.endswith('txt'), files)
+
+data = []
+for file in files:
+    with open (file, "r") as myfile:
+        d=myfile.read().replace('\n', '')
+        d = unicode(d, errors='ignore')
+        data.append(tb(d))
+
+bloblist = data
+
+print('for blob')
+#import pdb;pdb.set_trace()
+for blob in bloblist:
+    #print(len(blob.words))
+    blob.words_count = build_words_count(blob)
+    blob.words_len = len(blob.words)
+
+for i, blob in enumerate(bloblist):
+    print("Top words in document {}".format(i + 1))
+    #import pdb;pdb.set_trace()
+    scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    #for word, score in sorted_words[:30]:
+    #    print("{}: {}".format(word, round(score, 5)))
+    print(files[i], sorted_words[:20])
+
+
+print('exit')
